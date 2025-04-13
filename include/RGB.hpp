fd6140971fcbba479bc08c2a57f7c14e9bc0f063
Patryk Pęczak <ppeczak0405@gmail.com>
2025-04-13 23:42:22 +0200

    Made RGB class GPU compatible

--- include/RGB.hpp
+++ include/RGB.hpp
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <iostream>
 
+#include "CudaCompat.hpp"
+
 namespace PekiProc {
 class RGB;
 }
@@ -16,25 +18,25 @@ namespace PekiProc {
 
 class RGB {
  public:
-  RGB() : r(0), g(0), b(0) {}
+  CUDA_HD RGB() : r(0), g(0), b(0) {}
 
-  RGB(uint8_t R, uint8_t G, uint8_t B) : r(R), g(G), b(B) {}
+  CUDA_HD RGB(uint8_t R, uint8_t G, uint8_t B) : r(R), g(G), b(B) {}
 
   const static RGB WHITE, GRAY, BLACK, RED, GREEN, BLUE;
 
   static constexpr int RGB_SIZE = 3;
 
-  uint8_t getR() const { return r; }
+  CUDA_HD uint8_t getR() const { return r; }
 
-  uint8_t getG() const { return g; }
+  CUDA_HD uint8_t getG() const { return g; }
 
-  uint8_t getB() const { return b; }
+  CUDA_HD uint8_t getB() const { return b; }
 
-  void setR(uint8_t c) { r = c; }
+  CUDA_HD void setR(uint8_t c) { r = c; }
 
-  void setG(uint8_t c) { g = c; }
+  CUDA_HD void setG(uint8_t c) { g = c; }
 
-  void setB(uint8_t c) { b = c; }
+  CUDA_HD void setB(uint8_t c) { b = c; }
 
   friend std::ostream& ::operator<<(std::ostream& stream, const RGB& color);
 
